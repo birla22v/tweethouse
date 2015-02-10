3c09ba299f10c51a1ceb25efdd193fefd20d1037
brossetti1 <brossetti1@gmail>
2015-02-10 13:52:26 -0500

    added beginings of Shouts Controller, routing not working yet.

---
+++ db/migrate/20150209212247_create_shouts.rb
@@ -0,0 +1,10 @@
+class CreateShouts < ActiveRecord::Migration
+  def change
+    create_table :shouts do |t|
+      t.integer :user_id
+      t.text :body
+
+      t.timestamps null: false
+    end
+  end
+end
