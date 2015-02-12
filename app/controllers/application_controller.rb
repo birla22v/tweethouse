9e63751b882de8ff007b30bc46ab47cdf36620b6
Vedika Birla <birla22v@mtholyoke.edu>
2015-02-12 09:44:36 -0500

    check box working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :configure_sign_up_params, if: :devise_controller?
-
+  before_action :configure_update_params, if: :devise_controller?
   private
   def configure_sign_up_params
-    devise_parameter_sanitizer.for(:sign_up) << :username
+    devise_parameter_sanitizer.for(:sign_up) << :username << :private
+  end
+  def configure_update_params
+    devise_parameter_sanitizer.for(:account_update) << :private
   end
 end
